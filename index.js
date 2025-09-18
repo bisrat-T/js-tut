67d853654ee426b822fe3d4f846d234099676cd3
bisrat-T <141144577+bisrat-T@users.noreply.github.com>
2025-09-18 09:54:11 +0300

    first version
    this is the first version of the code

---
+++ index.js
@@ -0,0 +1,22 @@
+  let count= 0
+//   amo= document.getElementById("amount").innerText
+//   console.log(amo)
+   function add(){
+    count+=1
+   console.log(count)
+document.getElementById("amount").innerText= count
+   }
+   let saved=document.getElementById("amount").inn
+function save(){
+    console.log(count)
+}
+save()
+console.log(count)
+
+
+
+function per(){
+   console.log("error mm")
+   console.log(document.getElementById("error").innerText)
+   document.getElementById("error").innerText="this is error!"
+}
